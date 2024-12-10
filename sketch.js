85c656a959fc2e9926287d122e6a9c1a3faa848e
corbin Boughen <50217007+theBough@users.noreply.github.com>
2024-12-10 10:50:29 -0600

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,32 @@
+let grid = [];
+let size = 50;
+let numRows = 6;
+let numCols = 5;
+
+
+function setup() {
+  createCanvas(400, 400);
+  setGrid();
+  console.log(grid.length)
+}
+
+function draw() {
+  background(220);
+  displayGrid()
+}
+
+function setGrid(){
+  for(var i = 0; i<numRows; i++){
+    for(var j = 0; j<numCols; j++){
+      grid.push(new Cell(j*size,i*size,size,200,true))
+    }
+  }
+}
+
+function displayGrid(){
+  for(var i = 0; i<grid.length; i++){
+    
+    grid[i].display();
+    
+  }
+}
